010324c5f0ae15fcc5da169b2994bcc284fce723
marti-gutierrez <martigh14@gmail.com>
2023-02-14 10:58:57 -0600

    start: init project payload

---
+++ src/payload.config.ts
@@ -0,0 +1,22 @@
+import { buildConfig } from "payload/config";
+import path from "path";
+// import Examples from './collections/Examples';
+import Users from "./collections/Users";
+
+export default buildConfig({
+	serverURL: process.env.PAYLOAD_PUBLIC_BASE_DNS,
+	admin: {
+		user: Users.slug,
+	},
+	collections: [
+		Users,
+		// Add Collections here
+		// Examples,
+	],
+	typescript: {
+		outputFile: path.resolve(__dirname, "payload-types.ts"),
+	},
+	graphQL: {
+		schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
+	},
+});
